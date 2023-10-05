e41cb77a5779aa1ca61fe42ae9345fec4e0eb3d8
klaus <oriokiklaus@gmail.com>
2023-10-06 01:30:13 +0300

    make executable

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,19 +1,12 @@
 from typing import Tuple, List
-
 """
 Function returns a list of integers
 multiplied by specified factor
 passed as argument.
 """
 
+
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    """Returns list of integers multiplied by particular factor.
-    Args:
-        lst: A tuple of integers.
-        factor: An integer.
-    Returns:
-        A list of integers.
-    """
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
